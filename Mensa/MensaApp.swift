48c4e984d7c9a26783e00b6f762ffc6995c384e5
Timo Reusch <mail@timo-reusch.de>
2022-08-25 15:50:02 +0200

    Initial Commit

---
+++ Mensa/MensaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MensaApp.swift
+//  Mensa
+//
+//  Created by Timo Reusch on 25.08.22.
+//
+
+import SwiftUI
+
+@main
+struct MensaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
